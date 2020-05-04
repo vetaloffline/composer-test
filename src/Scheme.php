352c5d5c623c91b25b25af76e2886a6ca1d3e047
Vitaliy <motilevskiy_v@itstep.org>
2020-05-04 17:04:56 +0300

    first commit

---
+++ src/Scheme.php
@@ -0,0 +1,14 @@
+<?php
+
+class Scheme
+{
+    public function isFirstBigger($itemOne, $itemTwo)
+    {
+        return $itemOne > $itemTwo;
+    }
+
+    public static function lowerText($text)
+    {
+        return strtolower($text);
+    }
+}
